b8424d833a943f33f79c6e3cd452f5ba8e0be648
ducquan135 <ducquan135@live.com>
2016-11-29 16:10:54 +0700

    new warehouse

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,3 +3,4 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
 Yii::setAlias('@cmsapi', dirname(dirname(__DIR__)) . '/cmsapi');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@warehouse', dirname(dirname(__DIR__)) . '/warehouse');
