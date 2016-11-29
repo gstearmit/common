6e30a6cf599dc258e9292bb1b5da641bb152b808
ducquan135 <ducquan135@live.com>
2016-11-29 15:52:20 +0700

    optimize import

--- tests/unit/models/LoginFormTest.php
+++ tests/unit/models/LoginFormTest.php
@@ -2,9 +2,9 @@
 
 namespace common\tests\unit\models;
 
-use Yii;
-use common\models\LoginForm;
 use common\fixtures\User as UserFixture;
+use common\models\LoginForm;
+use Yii;
 
 /**
  * Login form test
